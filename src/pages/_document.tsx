37bbf133a08706823f371d9710ec2b1090b139db
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2022-07-04 00:45:39 -0300

    feat: load styles in server site too

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,25 +17,73 @@
  */
 
 import React from 'react';
-import {Head, Html, Main, NextScript} from 'next/document';
 
-const Document = () => {
-  return (
-    <Html>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true'/>
-        <link
-          href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@300;400;600;700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <body>
-        <Main/>
-        <NextScript/>
-      </body>
-    </Html>
-  );
-};
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
 
-export default Document;
+import {ServerStyleSheet} from 'styled-components';
+
+/** App custom Next.js document */
+class AppDocument extends Document {
+  /**
+   * Get the initial props of the document with styled-components
+   *
+   * @param {DocumentContext} ctx
+   */
+  public static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  /**
+   * Renders the document in SSR
+   *
+   * @return {JSX.Element}
+   */
+  public render(): JSX.Element {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true'/>
+          <link
+            href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@300;400;600;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main/>
+          <NextScript/>
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default AppDocument;
