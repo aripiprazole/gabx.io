05e4848669f895d3214ab0eaad8f5b0af6b6daa1
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2023-02-22 20:29:21 -0300

    feat: add sass

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 
   images: {
     domains: ['raw.githubusercontent.com'],
+  },
+
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src', 'styles')],
   },
 
   webpack(config) {
