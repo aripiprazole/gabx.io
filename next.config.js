26f6c619b81c73bf7fadf903cd72d594540d86ec
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2023-03-23 11:44:37 -0300

    fix: fix webpack sass config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,9 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 
   images: {
     domains: ['raw.githubusercontent.com'],
-  },
-
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'src', 'styles')],
   },
 
   webpack(config) {
