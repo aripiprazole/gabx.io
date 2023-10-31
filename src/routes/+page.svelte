9763704f3da7df9c1fc1c677b9c7c39d7f0b987b
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2023-10-31 20:31:47 -0300

    feat: add boilerplate

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,56 @@
-<div></div>
+<script>
+  import Layout from 'lib/Layout.svelte';
+  import Section from 'lib/Section.svelte';
+</script>
+
+<svelte:head>
+  <title>Hi, I&apos;m Gabi!</title>
+  <meta property="og:title" content="Hi, I'm Gabi! 👋" />
+  <meta
+    name="description"
+    content="My name is Gabrielle and I am a 17-year-old software engineer with a passion for functional programming and the Kotlin programming language. In my free time, I enjoy playing Minecraft and working on projects related to compilers. I am excited to share my skills and experience with others in the field."
+  />
+  <meta
+    property="og:description"
+    content="My name is Gabrielle and I am a 17-year-old software engineer with a passion for functional programming and the Kotlin programming language. In my free time, I enjoy playing Minecraft and working on projects related to compilers. I am excited to share my skills and experience with others in the field."
+  />
+  <meta
+    property="og:image"
+    content="https://raw.githubusercontent.com/aripiprazole/aripiprazole/main/profile.png"
+  />
+  <meta property="og:image:width" content="1200" />
+  <meta property="og:image:height" content="1200" />
+</svelte:head>
+
+<Layout>
+  <h1>Hi, I&apos;m Gabi! 👋</h1>
+  <p>
+    My name is Gabrielle and I am a 17-year-old software engineer with a passion
+    for functional programming and the Kotlin programming language. In my free
+    time, I enjoy playing Minecraft and working on projects related to
+    compilers. I am excited to share my skills and experience with others in the
+    field.
+  </p>
+
+  <Section>
+    <h2>My projects 🧪</h2>
+    <p>
+      I'm passionate about compilers, type theory, the Minecraft protocol, and
+      Hindley Milner! These topics perfectly blend my love for programming and
+      problem-solving. Compilers transform code into machine code, while Hindley
+      Milner adds elegance and safety to the process. Tinkering with the
+      Minecraft protocol to optimize and customize gameplay is a joy. I love
+      pushing the boundaries of programming and seeking new challenges!
+    </p>
+  </Section>
+
+  <Section>
+    <h2>My work ️❤️️</h2>
+    <p>
+      I specialize in web site and working with the Minecraft protocol. With
+      years of experience in software development, I have a passion for creating
+      efficient and reliable tools for programming and game development. Let's
+      bring your ideas to life!
+    </p>
+  </Section>
+</Layout>
